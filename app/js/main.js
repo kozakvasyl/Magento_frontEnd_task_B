685d1523cd0eb41b892209b52d3c3cd93e855f7c
kozakvasyl <koretsua@gmail.com>
2019-11-07 04:44:33 +0200

    implemented google map

--- app/js/main.js
+++ app/js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
     infinite: true,
     speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
     responsive: [
       {
         breakpoint: 768,
@@ -37,18 +37,47 @@ $(document).ready(function(){
 });
 
 //isotope-------------------------------
-$('.work-items').isotope({
-  itemSelector: '.item',
-  layoutMode: 'fitRows'
+$('.grid').isotope({
+  itemSelector: '.grid-item',
+  masonry: {
+    columnWidth: 460,
+    gutter: 20
+  }
 });
 $('.work-meniu li').click(function() {
   $('.work-meniu li').removeClass('active');
   $(this).addClass('active');
 
   var selector = $(this).attr('data-filter');
-  $('.work-items').isotope({
+  $('.grid').isotope({
     filter: selector
   });
   return false;
 });
 
+var description = $('.description');
+$('.grid-item').each(function() {
+  var classNames = $(this).attr('class').split(' ');
+  classNames.shift();
+  console.log(classNames.join('/'));
+  
+  description.text(classNames.join('/'));
+});
+
+/* $('.description').each(function() {
+var classNames = $('.grid-item').attr('class').split(' ');
+classNames.shift();
+  console.log(classNames.join('/'));
+  $(this).text(classNames.join('/'));
+}); */
+
+//GOOGLE MAP----------------------------------------
+var map;
+function initMap() {
+  var opt = {
+    center: {lat: 51.765658, lng: 25.504076},
+    zoom: 14,
+    mapTypeId: 'satellite'
+  };
+  map = new google.maps.Map(document.getElementById('map'), opt);
+}
