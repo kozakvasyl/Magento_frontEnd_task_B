e8e7357c92805585653453be678103ef587e13b5
kozakvasyl <koretsua@gmail.com>
2019-11-08 03:02:38 +0200

    added dist directory

--- dist/js/fullpage_script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-        anchors: ['block1', 'block2', 'block3'],
-        menu: '#menu',
-        css3: true,
-        scrollingSpeed: 1000
-    });
-});
