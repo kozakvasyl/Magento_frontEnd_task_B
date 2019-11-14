2a37d6db3d46a276523f5e2fdd1cdd0df6685dae
kozakvasyl <koretsua@gmail.com>
2019-11-14 20:41:33 +0200

    styled deals section with slider

--- app/js/main.js
+++ app/js/main.js
@@ -24,13 +24,23 @@ $('#searsh').on('keyup', function() {
 //slick--------------------------------
 $(document).ready(function(){
   $('.slider').slick({
-    dots: true,
-    arrows: false,
+    dots: false,
+    arrows: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
+    variableWidth: true,
+    mobileFirst: true,
+    prevArrow: '<div class="prev"></div>',
+    nextArrow: '<div class="next"></div>',
     responsive: [
+      {
+        breakpoint: 530,
+        settings: {
+          slidesToShow: 2
+        }
+      },
       {
         breakpoint: 768,
         settings: {
@@ -38,9 +48,9 @@ $(document).ready(function(){
         }
       },
       {
-        breakpoint: 520,
+        breakpoint: 1120,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 4
         }
       }
     ]
